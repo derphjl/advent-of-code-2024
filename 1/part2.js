87200f25bb16ed3453e9e50530321d8e5be8602f
flip <github@phjl.de>
2024-12-04 22:27:29 +0100

    solved part2 in node

--- 1/part2.js
+++ 1/part2.js
@@ -3,27 +3,28 @@ import { readFile } from 'node:fs/promises';
 console.log('🎅 Advent of Code 2024, Day 1!\n');
 
 try {
-  const inputFileContent = (await readFile('input-example.txt', { encoding: 'utf8' })).trim();
+  const inputFileContent = (await readFile('input.txt', { encoding: 'utf8' })).trim();
   const inputFileLines = inputFileContent.split('\n');
-
+  
+  //this loop fetches the integers of the left and right list and puts them in an array
   let leftList = [];
   let rightList = [];
-
-  //fill the lists!
   for (let line of inputFileLines) {
-    const leftInteger = line.match(/^\d+?(?=\ )/g);
-    const rightInteger = line.match(/\d+?$/g);
-
+    const leftInteger = parseInt(line.match(/^\d+?(?=\ )/g)[0]);
+    const rightInteger = parseInt(line.match(/\d+?$/g)[0]);
     leftList.push(leftInteger);
     rightList.push(rightInteger);
   }
-
-console.log("LeftList");
-console.log(leftList);
-console.log("Right List");
-console.log(rightList);
-
   
+  //this loop checks for every iterm in the left list: how often does it appear in the right list and does the required maths
+  let runningValue = 0;
+  for (let leftListItem of leftList) {
+    let leftListItemQuantity = rightList.filter((a) => a == leftListItem).length;
+    runningValue += ( leftListItem * leftListItemQuantity );
+  }
+
+  console.log(runningValue);
+
 } catch (error) {
   console.error('there was an error:', error.message);
 }
